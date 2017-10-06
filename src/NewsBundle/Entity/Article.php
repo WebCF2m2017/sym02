ad25de9a1048932c41723b7c0f7bc9219f043314
michaeljpitz <michaeljpitz@gmail.com>
2017-10-06 12:15:12 +0200

    création des entités venant de la db avec doctrine:mapping:import, puis création des getters et setters avec donctrine:generate:entities

---
+++ src/NewsBundle/Entity/Article.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article", indexes={@ORM\Index(name="fk_article_author_idx", columns={"authorId"})})
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="theTitle", type="string", length=100, nullable=false)
+     */
+    private $thetitle;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="theText", type="text", length=65535, nullable=true)
+     */
+    private $thetext;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="theDate", type="datetime", nullable=true)
+     */
+    private $thedate;
+
+    /**
+     * @var \Author
+     *
+     * @ORM\ManyToOne(targetEntity="Author")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="authorId", referencedColumnName="id")
+     * })
+     */
+    private $authorid;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set thetitle
+     *
+     * @param string $thetitle
+     *
+     * @return Article
+     */
+    public function setThetitle($thetitle)
+    {
+        $this->thetitle = $thetitle;
+
+        return $this;
+    }
+
+    /**
+     * Get thetitle
+     *
+     * @return string
+     */
+    public function getThetitle()
+    {
+        return $this->thetitle;
+    }
+
+    /**
+     * Set thetext
+     *
+     * @param string $thetext
+     *
+     * @return Article
+     */
+    public function setThetext($thetext)
+    {
+        $this->thetext = $thetext;
+
+        return $this;
+    }
+
+    /**
+     * Get thetext
+     *
+     * @return string
+     */
+    public function getThetext()
+    {
+        return $this->thetext;
+    }
+
+    /**
+     * Set thedate
+     *
+     * @param \DateTime $thedate
+     *
+     * @return Article
+     */
+    public function setThedate($thedate)
+    {
+        $this->thedate = $thedate;
+
+        return $this;
+    }
+
+    /**
+     * Get thedate
+     *
+     * @return \DateTime
+     */
+    public function getThedate()
+    {
+        return $this->thedate;
+    }
+
+    /**
+     * Set authorid
+     *
+     * @param \NewsBundle\Entity\Author $authorid
+     *
+     * @return Article
+     */
+    public function setAuthorid(\NewsBundle\Entity\Author $authorid = null)
+    {
+        $this->authorid = $authorid;
+
+        return $this;
+    }
+
+    /**
+     * Get authorid
+     *
+     * @return \NewsBundle\Entity\Author
+     */
+    public function getAuthorid()
+    {
+        return $this->authorid;
+    }
+}
---
+++ src/NewsBundle/Entity/Author.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace NewsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Author
+ *
+ * @ORM\Table(name="author")
+ * @ORM\Entity
+ */
+class Author
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=80, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surName", type="string", length=80, nullable=true)
+     */
+    private $surname;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Author
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return Author
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
